bdd765dcb1ecda592547b9765afdf93f9f4c126f
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:21:06 -0500

    Create zero_trust_access_identity_provider.import.tf

---
+++ pbdz/zero_trust_access_identity_provider.import.tf
@@ -0,0 +1,14 @@
+import {
+  to = cloudflare_zero_trust_access_identity_provider.onetimepin
+  id = "accounts/2342c30bd94a1fd213123e21aec3167d/2e83bb71-cb87-4e58-b02a-b811560a8bd1"
+}
+
+import {
+  to = cloudflare_zero_trust_access_identity_provider.google_oauth
+  id = "accounts/2342c30bd94a1fd213123e21aec3167d/424eba98-23bd-49c1-b835-0678dacb99a9"
+}
+
+import {
+  to = cloudflare_zero_trust_organization.pbdz
+  id = "2342c30bd94a1fd213123e21aec3167d"
+}
