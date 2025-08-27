da100fbf4acb55a34378b3f5ab8a16f63a861573
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:09:37 -0500

    Create email_routing.pbdz.xyz.import.tf

---
+++ pbdz/email_routing.pbdz.xyz.import.tf
@@ -0,0 +1,9 @@
+import {
+  to = cloudflare_email_routing_rule.forward_to_pobudz_gmail
+  id = "5e39558b5935c9c22b71a24b248b7a0a/d72cf83a4df44d9d906c0ca1c6dbfa6b"
+}
+
+import {
+  to = cloudflare_email_routing_settings.terraform_managed_resource_5e39558b5935c9c22b71a24b248b7a0a_0
+  id = "5e39558b5935c9c22b71a24b248b7a0a"
+}
