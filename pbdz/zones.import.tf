36dde5bc1d5b45c9ced6f1a7480b54a88ab4b42c
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:44:34 -0500

    Create zones.import.tf

---
+++ pbdz/zones.import.tf
@@ -0,0 +1,24 @@
+import {
+  to = cloudflare_zone.nutanixpro_com
+  id = "9481528e6a982bbf0e531ebb5e7a5647"
+}
+
+import {
+  to = cloudflare_zone.pbdz_xyz
+  id = "5e39558b5935c9c22b71a24b248b7a0a"
+}
+
+import {
+  to = cloudflare_zone.pobudz_net
+  id = "e9182fb3ec62f3cb71a6aed02a6243d3"
+}
+
+import {
+  to = cloudflare_zone.tier3_app
+  id = "4f9d6d27af516ded19447667316e5381"
+}
+
+import {
+  to = cloudflare_zone.yetanothermeme_com
+  id = "8ad0bac6de785263cb4a5696bf32c43d"
+}
