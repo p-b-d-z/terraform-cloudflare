aa396143b4e18be0c181d2cdb53ee0f607359b1e
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:05:01 -0500

    Create dns.pbdz.xyz.import.tf

---
+++ pbdz/dns.pbdz.xyz.import.tf
@@ -0,0 +1,24 @@
+import {
+  to = cloudflare_dns_record.pbdz_xyz_mx_tertiary
+  id = "5e39558b5935c9c22b71a24b248b7a0a/11f99c721464430220cc5724ec04979f"
+}
+
+import {
+  to = cloudflare_dns_record.pbdz_xyz_mx_secondary
+  id = "5e39558b5935c9c22b71a24b248b7a0a/132abdd4cd1ec5c489121cff94dc9673"
+}
+
+import {
+  to = cloudflare_dns_record.pbdz_xyz_mx_primary
+  id = "5e39558b5935c9c22b71a24b248b7a0a/c845a685dfd032d9f58e077124153b4c"
+}
+
+import {
+  to = cloudflare_dns_record.pbdz_xyz_dmarc
+  id = "5e39558b5935c9c22b71a24b248b7a0a/1f549d710aebe0801b3cdfe0038943c6"
+}
+
+import {
+  to = cloudflare_dns_record.pbdz_xyz_spf
+  id = "5e39558b5935c9c22b71a24b248b7a0a/5b34296bf5ef8f9d13486b5fa9d49900"
+}
