a0c4b2df208f993a532d827c8f2c426c75935b7f
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:22:29 -0500

    Create zero_trust_access_policy.import.tf

---
+++ pbdz/zero_trust_access_policy.import.tf
@@ -0,0 +1,34 @@
+import {
+  to = cloudflare_zero_trust_access_policy.allow_uptime_kuma
+  id = "2342c30bd94a1fd213123e21aec3167d/7ead921a-8afe-4d35-bea2-537a57e4af98"
+}
+
+import {
+  to = cloudflare_zero_trust_access_policy.allow_public_access
+  id = "2342c30bd94a1fd213123e21aec3167d/d57cb389-c861-4fe0-b7b0-e026a25a412c"
+}
+
+import {
+  to = cloudflare_zero_trust_access_policy.permit_pbdz_xyz
+  id = "2342c30bd94a1fd213123e21aec3167d/c664143f-35cc-4901-9687-17735f2b1cc9"
+}
+
+import {
+  to = cloudflare_zero_trust_access_policy.permit_google_auth
+  id = "2342c30bd94a1fd213123e21aec3167d/2f696a4d-4875-4c3a-acc8-5788be25c8aa"
+}
+
+import {
+  to = cloudflare_zero_trust_access_policy.google_auth_vault
+  id = "2342c30bd94a1fd213123e21aec3167d/55876fa2-f040-4e27-8df6-59898632fdd8"
+}
+
+import {
+  to = cloudflare_zero_trust_access_policy.google_auth_jenkins
+  id = "2342c30bd94a1fd213123e21aec3167d/be685e92-f836-4e2d-8d1f-164e065e8540"
+}
+
+import {
+  to = cloudflare_zero_trust_access_policy.google_auth_linuxserver
+  id = "2342c30bd94a1fd213123e21aec3167d/f7dbdf6f-5de3-4a3e-91e2-972d8a21ed3c"
+}
