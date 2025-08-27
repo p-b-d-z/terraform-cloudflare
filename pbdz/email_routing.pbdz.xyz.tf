47ab913a2fc409e8595d802742676c2fa84dd291
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:16:33 -0500

    Create email_routing.pbdz.xyz.tf

---
+++ pbdz/email_routing.pbdz.xyz.tf
@@ -0,0 +1,19 @@
+resource "cloudflare_email_routing_rule" "forward_to_pobudz_gmail" {
+  enabled  = true
+  name     = "Rule created at 2022-05-01T06:05:07.477Z"
+  priority = 0
+  zone_id  = cloudflare_zone.pbdz_xyz.id
+  actions = [{
+    type  = "forward"
+    value = ["${var.cloudflare_dmarc_email}"]
+  }]
+  matchers = [{
+    field = "to"
+    type  = "literal"
+    value = "${var.joe_pbdz_xyz}"
+  }]
+}
+
+resource "cloudflare_email_routing_settings" "terraform_managed_resource_5e39558b5935c9c22b71a24b248b7a0a_0" {
+  zone_id = cloudflare_zone.pbdz_xyz.id
+}
