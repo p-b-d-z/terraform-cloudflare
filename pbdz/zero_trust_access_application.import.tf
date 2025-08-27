b23733597546854bb56c6f87cc687ca5e10c51b7
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:18:41 -0500

    Create zero_trust_access_application.import.tf

---
+++ pbdz/zero_trust_access_application.import.tf
@@ -0,0 +1,54 @@
+import {
+  to = cloudflare_zero_trust_access_application.jenkins_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/939a73b0-d34f-4ebc-99be-fd53a03541e1"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.sabnzbd_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/67913678-56c9-47a5-9529-ac844a1eba2f"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.radarr_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/3ec5bf4d-164f-4f15-a218-f95b4c73abfe"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.sonarr_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/781d6a30-2dbd-4a3b-9b24-450ccffee842"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.vault_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/9b34b8bc-5b25-48da-9395-3f105efbaa97"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.teslamate_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/8f0a6d2b-367f-4dec-b3e0-759bd89cbb9a"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.uptime_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/4c3f401f-56d1-4806-9feb-e52b7ead6e46"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.pwpusher_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/fb196c1c-7162-4ff0-ac33-bea95b8ea7b4"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.ssh3_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/9c27ed63-6097-43b4-bdab-f7aa6166a9cd"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.ssh2_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/6aae1c46-f850-445b-a3b5-2427d83c141b"
+}
+
+import {
+  to = cloudflare_zero_trust_access_application.ssh1_pbdz_xyz
+  id = "zones/5e39558b5935c9c22b71a24b248b7a0a/0c98f2b3-54c4-4cad-bc50-ff2ce92f0c73"
+}
