3cd2025f27f00ab20f0bce51dce1a102d855d2fe
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 17:10:47 -0500

    Update account.tf

--- pbdz/account.tf
+++ pbdz/account.tf
@@ -2,7 +2,7 @@ resource "cloudflare_account" "master" {
   name = "Pobuda Estates LLC"
   type = "standard"
   settings = {
-    abuse_contact_email    = var.cloudflare_abuse_email
+    abuse_contact_email    = var.email_contacts.abuse
     enforce_twofactor      = false
   }
 }
