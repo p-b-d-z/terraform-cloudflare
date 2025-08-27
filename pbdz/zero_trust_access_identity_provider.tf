f9048315d53da5a3031a2b9d36c1f5cb8fcafecb
joe <104695668+p-b-d-z@users.noreply.github.com>
2025-08-27 16:21:48 -0500

    Create zero_trust_access_identity_provider.tf

---
+++ pbdz/zero_trust_access_identity_provider.tf
@@ -0,0 +1,32 @@
+resource "cloudflare_zero_trust_access_identity_provider" "onetimepin" {
+  name       = "onetimepin"
+  type       = "onetimepin"
+  account_id = cloudflare_account.master.id
+  config     = {}
+}
+
+resource "cloudflare_zero_trust_access_identity_provider" "google_oauth" {
+  name        = "Google"
+  type        = "google"
+  account_id  = cloudflare_account.master.id
+  config      = {
+    client_id = "937008270753-rvqbpbrnlknlctuvq65d95c01o3b6k51.apps.googleusercontent.com"
+  }
+  scim_config = {
+    enabled                  = false
+    group_member_deprovision = false
+    identity_update_behavior = "no_action"
+    seat_deprovision         = false
+    user_deprovision         = false
+  }
+}
+
+resource "cloudflare_zero_trust_organization" "pbdz" {
+  allow_authenticate_via_warp = false
+  auth_domain                 = var.cloudflare_zero_trust_access_domain
+  is_ui_read_only             = false
+  name                        = var.cloudflare_zero_trust_access_domain
+  warp_auth_session_duration  = "24h"
+  account_id                  = cloudflare_account.master.id
+  login_design                = {}
+}
